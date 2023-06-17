54274f3046a6f039d592367a7aea9629e58a85c8
Oleksandr Koval <timew2762@gmail.com>
2023-06-17 22:09:15 +0300

    fix works section

--- src/works/Works.js
+++ src/works/Works.js
@@ -12,6 +12,7 @@ const Works = () => {
                 <div className={s.works}>
                     <Work title={'Social'} description={'This is my pet project, will would show my skills'}/>
                     <Work title={'Shop'} description={'This is my pet project, which will show my skills'}/>
+                    <Work title={'Todolist'} description={'This is my pet project, which will show my skills'}/>
                 </div>
             </div>
         </div>
